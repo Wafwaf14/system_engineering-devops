2f59f94abb97fd2e40e2915b5bd9923db7f4a142
Wafaa HIJARY <95526772+Wafwaf14@users.noreply.github.com>
2024-01-19 23:13:44 +0100

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,12 +1,7 @@
 # 2-execute_a_command.pp
 
 # Define an exec resource to kill the "killmenow" process
-exec { 'killmenow':
-  command     => '/usr/bin/pkill -f killmenow',
-  refreshonly => true,
-}
-
-# Example to notify the exec resource when needed (you can customize this based on your needs)
-notify { 'Terminate killmenow process':
-  require => Exec['killmenow'],
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
